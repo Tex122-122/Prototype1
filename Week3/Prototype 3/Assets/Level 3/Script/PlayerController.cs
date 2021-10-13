d2373fce6ee8a367e429fdaa2c277441bce88c7d
Brandon <1020232@student.sae.edu.au>
2021-10-13 21:44:36 +1100

    controller

---
+++ Week3/Prototype 3/Assets/Level 3/Script/PlayerController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] Transform playerCamera = null;
+    [SerializeField] float mouseSensitivity = 3.5f;
+
+    [SerializeField] bool lockCursor = true;
+
+    float cameraPitch = 0.0f;
+
+    void Start()
+    {
+        if(lockCursor)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+
+    void Update()
+    {
+        UpdateMouseLook();
+    }
+
+    void UpdateMouseLook()
+    {
+        Vector2 mouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+
+        cameraPitch -= mouseDelta.y * mouseSensitivity;
+
+        cameraPitch = Mathf.Clamp(cameraPitch, -90.0f, 90.0f);
+
+        playerCamera.localEulerAngles = Vector3.right * cameraPitch;
+
+        transform.Rotate(Vector3.up * mouseDelta * mouseSensitivity);
+    }
+}
+
