29d205f37df38e513fa48e1d673db8d8c20c028f
Brandon <1020232@student.sae.edu.au>
2021-10-25 15:29:29 +1100

    Level 1

--- Week3/Prototype 3/Assets/Week5/Script/jump.cs
+++ Week3/Prototype 3/Assets/Week5/Script/jump.cs
@@ -2,17 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class jump : MonoBehaviour
+public class Jump : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody rb;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector3 vel = rb.velocity;
+        vel.x = 5;
+        rb.velocity = vel;
+        if (Physics.Raycast(transform.position, Vector3.down, GetComponent<BoxCollider>().size.y / 2 + 0.4f))
+        {
+            Quaternion rot = transform.rotation;
+            rot.z = Mathf.Round(rot.z / 90) * 90;
+            transform.rotation = rot;
+            if (Input.GetMouseButtonDown(0))
+            {
+                rb.velocity = Vector3.zero;
+                rb.AddForce(Vector2.up * 55000);
+            }
+        }
+        else
+        {
+            transform.Rotate(Vector3.back * 5f);
+        }
     }
 }
+
