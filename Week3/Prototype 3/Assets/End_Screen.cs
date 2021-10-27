d37092fe26fc6aeb92e9e64570fffb40ae5823f8
Brandon <1020232@student.sae.edu.au>
2021-10-27 20:30:22 +1100

    final thing done

---
+++ Week3/Prototype 3/Assets/End_Screen.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class End_Screen : MonoBehaviour
+{
+    public GameObject EndScreen;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            EndScreen.gameObject.SetActive(true);
+        }
+    }
+}
